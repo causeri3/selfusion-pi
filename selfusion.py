8d185849aebccd4230bcaddae82c1c8272475b5e
causeri3 <vanessaca@web.de>
2025-07-18 16:32:39 +0200

    make prompt changeable via api request (bornhack)

--- selfusion.py
+++ selfusion.py
@@ -1,4 +1,6 @@
 import logging
+import uvicorn
+import threading
 
 from selfusion_utils.transformation import Transformation
 from selfusion_utils.args import get_args
@@ -7,5 +9,12 @@ args, unknown = get_args()
 
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
+def start_api():
+    uvicorn.run("selfusion_utils.prompt:app", host="127.0.0.1", port=8000)
+
+
 if __name__ == "__main__":
+    api_thread = threading.Thread(target=start_api, daemon=True)
+    api_thread.start()
+
     Transformation().run()
